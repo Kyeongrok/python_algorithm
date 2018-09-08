73cca9af7e21cc329ea2ccf3b19091b55f673dd7
ydseo <ydseo2015@gmail.com>
2018-09-08 14:50:48 +0900

    check

---
+++ main2.py
@@ -0,0 +1,17 @@
+def solution(array):
+    different = 0
+    sorted_array = sorted(array)
+    for i, j in zip(array, sorted_array):
+        if(i != j):
+            different += 1
+
+    if(different == 2):
+        result = True
+    else:
+        result = False
+
+    return result
+
+if __name__ == "__main__":
+    array = [2, 8, 7, 7, 8, 8, 8, 8, 5, 8, 8, 13, 15]
+    print("%s %r" % ('The answer is', solution(array)))
