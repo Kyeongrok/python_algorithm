c166eeef792e0c43008ecedf9d58193ab03b6d9f
mattheu <matthe@plutusds.com>
2018-10-18 11:03:49 +0900

    add bubble sort

--- com/chapter02_sort/01_select.py
+++ com/chapter02_sort/01_select.py
@@ -1,4 +1,4 @@
-def findSmallest(arr):
+def findSmallestIndex(arr):
     smallest = arr[0]
     smallest_index = 0
 
@@ -9,10 +9,10 @@ def findSmallest(arr):
     return smallest_index
 
 def selectionSort(arr):
-    newArr = []
+    resultArr = []
     for i in range(len(arr)):
-        smallest = findSmallest(arr)
-        newArr.append(arr.pop(smallest))
-    return newArr
+        smallest = findSmallestIndex(arr)   # 가장 작은 숫자의 인덱스를 찾는다.
+        resultArr.append(arr.pop(smallest))    # 해당 인덱스에서 뽑아다가 resultArr에 넣는다.
+    return resultArr
 
 print(selectionSort([5, 3, 6, 2, 10]))
