086264dac1f32ef2b94448c721a2f96f081c55a2
kyeongrok <oceanfog1@gmail.com>
2018-11-13 00:48:11 +0900

    simple search lecture

---
+++ lecture/simple_search/01_start.py
@@ -0,0 +1,11 @@
+arr = [1, 2, 3, 5, 6, 7, 8, 9, 10, 11]
+
+# 8은 몇번째에 있을까요?
+# 7번째 # 인덱스로는 6번째
+# 입력받은 숫자가 몇번째 인덱스에 있는지
+# 찾는 function만들기
+
+def simpleSearch(arr, targetNum):
+    return -1
+
+print(simpleSearch(arr, 8))
---
+++ lecture/simple_search/03_index_value.py
@@ -0,0 +1,8 @@
+arr = [1, 2, 3, 5, 6, 7, 8, 9, 10, 11]
+
+def simpleSearch(arr, targetNum):
+    for index in range(0, len(arr)):
+        print(arr[index])
+    return -1
+
+print(simpleSearch(arr, 8))
