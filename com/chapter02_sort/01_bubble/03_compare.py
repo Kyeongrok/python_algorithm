249cbe19e2e388b0046cb7ab1d41d6f74d37ee71
kyeongrok <oceanfog1@gmail.com>
2018-11-18 11:39:56 +0900

    pow

--- com/chapter02_sort/01_bubble/03_compare.py
+++ com/chapter02_sort/01_bubble/03_compare.py
@@ -1,6 +1,4 @@
 numbers = [7, 3, 2, 9]  # => [2, 3, 7, 9]
-
 first = numbers[0]
 second = numbers[1]
-
 print(first > second)
--- lecture/08_enumerate.py
+++ lecture/08_enumerate.py
@@ -1,9 +1,6 @@
 arr = [7, 3, 2, 9]
-
 for i, j in enumerate(arr):
     print(i, j)
 
-
 print(list(enumerate(arr)))
-
 print(arr[:2])
