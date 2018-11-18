716414e31cd5486a5b2ad672135c5381418328d2
go0win <gys3485@gmail.com>
2018-11-18 12:09:55 +0900

    st

---
+++ com/st/05_stringTokenizer_each.py
@@ -0,0 +1,10 @@
+string = "13+24*(35-46.76)"
+#["13", "+", "24", "*", "(", "35", "-", "46.76", ")"]
+
+def stringTokenizer(string):
+   for chr in string:
+       print(chr)
+   return[]
+
+result=stringTokenizer(string)
+print("=>",result)
---
+++ com/st/06_append.py
@@ -0,0 +1,11 @@
+string = "13+24*(35-46.76)"
+#["13", "+", "24", "*", "(", "35", "-", "46.76", ")"]
+
+def stringTokenizer(string):
+   result=[]
+   for chr in string:
+       result.append(chr)
+   return result
+
+result=stringTokenizer(string)
+print("=>",result)
