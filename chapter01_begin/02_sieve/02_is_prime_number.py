8c54a74983bbc81dd57850da6c039c7216b63a6c
kyeongrok <oceanfog1@gmail.com>
2018-12-30 12:21:31 +0900

    mod

--- chapter01_begin/02_sieve/02_is_prime_number.py
+++ chapter01_begin/02_sieve/02_is_prime_number.py
@@ -1,7 +1,7 @@
-def is_prime_number(num):
+def isPrimeNumber(num):
     for i in range(2, num):
         if num % i == 0: return False
     return True
 
 for i in range(1, 100):
-    print("{} {}".format(i, is_prime_number(i)))
+    print("{} {}".format(i, isPrimeNumber(i)))
---
+++ documents/prime/check_whether_the_number_is_prime.md
@@ -0,0 +1,2 @@
+# Python program to check whether a number is Prime or not
+* [whether_prime](https://www.geeksforgeeks.org/python-program-to-check-whether-a-number-is-prime-or-not/)
