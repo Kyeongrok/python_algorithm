b91a73194b01cb4f7f765c11602868c12db7b1fe
go0win <gys3485@gmail.com>
2019-01-06 12:20:00 +0900

    simple search

--- chapter02_search/01_simple_search/02_one_by_one.py
+++ chapter02_search/01_simple_search/02_one_by_one.py
@@ -4,6 +4,8 @@ arr = [1,2,3,5,6,7,8,9,10,11]
 # 7번째 # 인덱스로는 6번째
 
 def simpleSearch(arr, targetNum):
+    for index in range(0, len(arr)):
+        print(index)
     return -1
 
 print(simpleSearch(arr,8))
--- chapter02_search/01_simple_search/03_index_value.py
+++ chapter02_search/01_simple_search/03_index_value.py
@@ -1,9 +1,8 @@
 arr = [1,2,3,5,6,7,8,9,10,11]
 
-# 8은 몇번째에 있을까요?
-# 7번째 # 인덱스로는 6번째
-
 def simpleSearch(arr, targetNum):
+    for index in range(0, len(arr)):
+        print(arr[index])
     return -1
 
 print(simpleSearch(arr,8))
