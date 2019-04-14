85873d017b4a45387c2950f8ad8fe90609dae7d5
kyeongrok <oceanfog1@gmail.com>
2019-04-14 23:46:25 +0900

    add sequence

--- chapter07_dynamic_programming/03_LCS/04_loop_sequence_column.py
+++ chapter07_dynamic_programming/03_LCS/04_loop_sequence_column.py
@@ -4,6 +4,11 @@ sequence2 = "DCABDC"
 
 # sequence1 * sequence2만큼의 list만들기
 memo = [[0]*len(sequence1)]*len(sequence2)
+
 for i in range(len(sequence2)):
     for j in range(len(sequence1)):
-        print(i, j, sequence2[i], sequence1[j])
+        seq2i = sequence2[i]
+        seq1j = sequence1[j]
+        if(i == 0 and seq2i == seq1j):
+            memo[i][j] = 1
+print(memo)
