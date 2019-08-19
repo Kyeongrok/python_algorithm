de5d2e8e52568447498502bca515f59c5043e52a
mattheu <mattheu@plutusds.com>
2019-08-19 15:17:42 +0900

    return

--- chapter07_dynamic_programming/06_min_cost_path/06_i_upper_j_upper.py
+++ chapter07_dynamic_programming/06_min_cost_path/06_i_upper_j_upper.py
@@ -2,7 +2,6 @@ def getMinCost(matrix):
     n = len(matrix)
     dp = [[0] * n for i in range(n)]
 
-    # i는 행, j는 열
     for i in range(n):
         for j in range(n):
             if i == 0 and j == 0:
@@ -20,7 +19,7 @@ def getMinCost(matrix):
     for row in dp:
         print(row)
 
-    return 0
+    return dp[n-1][n-1]
 
 matrix = [
     [1,3,2],
