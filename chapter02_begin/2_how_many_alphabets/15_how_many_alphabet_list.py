11fca79951263d7358bc0899105ae82866c279a1
kyeongrok <oceanfog1@gmail.com>
2020-03-12 22:28:40 +0800

    max min

--- chapter02_begin/2_how_many_alphabets/15_how_many_alphabet_list.py
+++ chapter02_begin/2_how_many_alphabets/15_how_many_alphabet_list.py
@@ -4,9 +4,9 @@ for char in string:
     if char.isalpha() == True:
         count_each[ord(char) - 97] += 1
 
-max = 0
+max = count_each[0]
 index = 0
-for i in range(len(count_each)):
+for i in range(1, len(count_each)):
     if max < count_each[i]:
         max = count_each[i]
         index = i
