1cf2b5ac55852c88c731cb1aa568226ea199a7d0
kyeongrok <oceanfog1@gmail.com>
2020-03-27 13:49:55 +0800

    coin change

---
+++ chapter07_dynamic_programming/07_coin_change/01_make_memo.py
@@ -0,0 +1,24 @@
+# coin changing
+coins = [1, 3, 4, 5]
+aim = 7
+
+memo = [[0] * (aim + 1) for _ in range(len(coins)) ]
+
+for i in range(len(coins)):
+    for j in range(aim + 1):
+        if j == 0:
+            memo[i][j] = 0
+        if i == 0:
+            memo[i][j] = j
+        if i != 0 and coins[i] < j:
+            above = memo[i-1][j]
+            memo[i][j] = min(above, memo[i][j-coins[i]] + 1)
+        elif coins[i] > j:
+            memo[i][j] = memo[i-1][j]
+        elif coins[i] == j:
+            memo[i][j] = 1
+
+
+
+for row in memo:
+    print(row)
