b4eeb69064d8336b5a72f4889e6e4757c612eafa
kyeongrok <oceanfog1@gmail.com>
2020-11-18 14:07:48 +0900

    complete coding test

--- test/kids_loop/22_grid.py
+++ test/kids_loop/22_grid.py
@@ -7,25 +7,56 @@ def sum_of_digit(num):
         sum = sum + remainder
     return sum
 
-memo = [[0] * 2000 for _ in range(2000)]
-dp = [None] * 2000
+def max_sum(num):
 
-area = 0
-for i in range(-1000, 1000):
-    for j in range(-1000, 1000):
-        if dp[abs(i)] == None:
-           dp[abs(i)] = sum_of_digit(abs(i))
-        if dp[abs(j)] == None:
-            dp[abs(j)] = sum_of_digit(abs(j))
+    # 27 = 999
+    # 23 = 994
+    # 22 = 993
+    # 9 9 = 18
+    # 9 2 = 11
+    # 9 1 = 10
+    # 9 0 = 9
+    # 0 8 = 8
+    quotient = num // 9
+    remainder = num % 9
 
-        if dp[abs(i)] + dp[abs(j)] <= 23:
-            memo[i+1000][j+1000] = 1
 
-for i in range(-1000, 1000):
-    for j in range(-1000, 1000):
-        pass
 
-print(area)
+
+def check_safe(limit):
+    dp = [None] * 2000
+    memo = [[0] * 2000 for _ in range(2000)]
+    for i in range(-1000, 1000):
+        for j in range(-1000, 1000):
+            if dp[abs(i)] == None:
+               dp[abs(i)] = sum_of_digit(abs(i))
+            if dp[abs(j)] == None:
+                dp[abs(j)] = sum_of_digit(abs(j))
+
+            if dp[abs(i)] + dp[abs(j)] <= limit:
+                memo[i+1000][j+1000] = 1
+    return memo
+
+def check_available(memo):
+    memo[1000][1000] = 2
+
+    is_changing = True
+    cnt = 0
+    while is_changing:
+        cnt += 1
+        is_changing = False
+        for y_idx in range(1, len(memo) - 1):
+            for x_idx in range(1, len(memo[y_idx]) - 1):
+                if memo[y_idx][x_idx] == 2:
+                    print('{}---safe---'.format(cnt))
+                    continue
+                elif memo[y_idx][x_idx] != 0 and 2 in [memo[y_idx-1][x_idx], memo[y_idx+1][x_idx], memo[y_idx][x_idx-1], memo[y_idx][x_idx+1]]:
+                    memo[y_idx][x_idx] = 2
+                    is_changing = True
+
+
+memo = check_safe(23)
+memo = check_available(memo)
 plt.imshow(memo)
 plt.gca().invert_yaxis()
 plt.show()
--- test/kids_loop/how_large_the_area_the_robot_can_access.py
+++ test/kids_loop/how_large_the_area_the_robot_can_access.py
@@ -1,51 +1,51 @@
-import numpy as np
 import matplotlib.pyplot as plt
-def make_a_coords(extent):
-    x_s = np.arange(-extent, extent+1, 1)
-    y_s = np.arange(-extent, extent+1, 1)
+def sum_of_digit(num):
+    sum = 0
+    while(num > 0):
+        remainder = num % 10
+        num = num // 10
+        sum = sum + remainder
+    return sum
 
-    ### Loop through each point and check if safe
-    coords = []
-    for y in y_s:
-        x_coords = []
-        for x in x_s:
-            coords_digits_sum = sum([int(i) for i in str(abs(x))]) + sum([int(i) for i in str(abs(y))])
-            if coords_digits_sum <= 23:
-                safe = True
+def check_safe(size, limit):
+    dp = [None] * size * 2
+    memo = [[0] * size * 2 for _ in range(size * 2)]
+
+    for i in range(-size, size):
+        for j in range(-size, size):
+            if dp[abs(i)] == None:
+                dp[abs(i)] = sum_of_digit(abs(i))
+            if dp[abs(j)] == None:
+                dp[abs(j)] = sum_of_digit(abs(j))
+
+            if dp[abs(i)] + dp[abs(j)] <= limit:
+                memo[i+size][j+size] = 1
+    return memo
+
+
+size = 800
+memo = check_safe(size, 23)
+
+memo[size][size] = 2
+safe_cnt = 1
+tos = [
+    [len(memo) - 1, len(memo) - 1, 1, 1], # increase x increase y
+    [1, 1, -1, -1], # increase x decrease y
+    [1, len(memo) - 1, -1, 1], # decrease x increase y
+    [len(memo) - 1, 1, 1, -1] # increase x decrease y
+]
+for to in tos:
+    for x in range(size, to[0], to[2]):
+        for y in range(size, to[1], to[3]):
+            if memo[y][x] == 2:
+                continue
+            elif memo[y][x] == 1 and 2 in [memo[y-1][x], memo[y+1][x], memo[y][x-1], memo[y][x+1]]:
+                safe_cnt += 1
+                memo[y][x] = 2
             else:
-                safe = False
-            x_coords.append(safe)
-        coords.append(x_coords)
-    return coords
+                memo[y][x] = 0
 
-
-extent = 750
-coords = make_a_coords(extent)
-
-print(coords)
-
-# ### Initialize the origin as `really_safe` point
-# coords[extent][extent] = 'really_safe' # Origin
-#
-# ### Iteratively check if a safe point is beside a `really_safe` point
-# changing = True
-# while changing == True:
-#     changing = False
-#     # Skip the first and last rows and columns because these are just paddings for looking up/down/left/right
-#     for y,x_coords in enumerate(coords[1:-1], start=1):
-#         for x,is_safe in enumerate(x_coords[1:-1], start=1):
-#             if is_safe == 'really_safe':
-#                 continue
-#             ### look up, down, left, right of the point being checked
-#             elif is_safe and ('really_safe' in [coords[y-1][x], coords[y+1][x], coords[y][x-1], coords[y][x+1]]):
-#                 coords[y][x] = 'really_safe'
-#                 changing = True
-#
-# ### Count the number of "really safe" points only
-# really_safe_points = [[1 if safety == 'really_safe' else 0 for safety in x_coords[1:-1]] for x_coords in coords[1:-1]]
-# plt.imshow(really_safe_points)
-# plt.gca().invert_yaxis()
-#
-# ### Exclude first and last rows and columns in total area calculation since those were not iterated over
-# plt.title(f"Accessible area: {sum([sum(i) for i in really_safe_points])}. Total area: {((extent-1)*2)**2}")
-# plt.show()
+print('answer:', safe_cnt)
+plt.imshow(memo)
+plt.gca().invert_yaxis()
+plt.show()
