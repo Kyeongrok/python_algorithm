b1aacbcb982f79b3234f554f8b453ca5fd6901db
Kyeongrok Kim <oceanfog1@gmail.com>
2021-03-12 17:29:32 +0900

    speed test

---
+++ stack/bracket_no_stack/10_speed_test.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+def solution(s):
+    cnt = 0
+    while '()' in s:
+        sp = s.split('()')
+        s = ''.join(sp)
+        cnt += 1
+    print('cnt:', cnt)
+    return len(s) == 0 # _는 가독성 때문에 씁니다. 파이썬에서 인식 안합니다.
+
+s = '(' * 5000000 + ')'*5000000
+print(f'len:{len(s)} start:', datetime.now())
+print('final:', solution(s))
+print('finish:', datetime.now())
