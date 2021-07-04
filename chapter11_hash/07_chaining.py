0cab174cc7e5d97eb5d29752dee11a6a43e5368e
ydseo <ydseo2015@gmail.com>
2021-07-04 22:05:37 +0900

    chaining concept

---
+++ chapter11_hash/07_chaining.py
@@ -0,0 +1,26 @@
+class HashTable:
+   def __init__(self):
+       self.size = 10
+       self.table = [[] for _ in range(self.size)]
+
+   def hash(self, name):
+       ascii_sum = 0
+       for word in name:
+           ascii_sum += ord(word)
+       return ascii_sum % self.size
+
+   def insert(self, name, value):
+       hashIndex = self.hash(name)
+       self.table[hashIndex].append((name, value))
+
+   def printTable(self):
+       for idx, value in enumerate(self.table):
+           print([idx, value])
+
+table = HashTable()
+table.insert("jisoo", 1000)
+table.insert("kavin", 2000)
+table.insert("sam", 3000)
+table.insert("hash kim", 4000)
+table.insert("mirror", 5000)
+table.printTable()
